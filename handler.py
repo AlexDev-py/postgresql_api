e7bdc180405c3a53f57ad9fde401289f82d9cf75
AlexDev-py <49647025+alex_dev-py@users.noreply.github.com>
2020-01-13 17:02:10 +0900

    start

---
+++ handler.py
@@ -0,0 +1,18 @@
+import sqlite3 as sql
+
+
+class RequestHandler:
+    def __init__(self, db_host):
+        self._connection = sql.connect(db_host)
+        self._cursor = self._connection.cursor()
+
+    def execute(self, request):
+        self._cursor.execute(request)
+        return self._cursor.fetchall()
+
+    def commit(self):
+        self._connection.commit()
+
+    def close(self):
+        self._cursor.close()
+        self._connection.close()
